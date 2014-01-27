a610add86b19fb54afbad7d1dde0ca7493656b6c
Christine Rondeau <christine@bluelimemedia.com>
2014-01-27 09:39:09 -0800

    Added snippet for functions.php
    add_theme_support must also be used for custom header to work.

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+/* Use add_theme_support() in the functions.php file to support custom headers, like so:
+
+
+/* Implement the Custom Header feature */
+   require( get_template_directory() . 'custom-header.php' );
